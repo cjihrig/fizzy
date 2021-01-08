cc286eb6eadb7e85e1f1d683fc82c5246ebeb63a
Alex Beregszaszi <alex@rtfs.hu>
2021-01-08 17:32:56 +0000

    f

--- test/unittests/bitcount_test_cases.hpp
+++ test/unittests/bitcount_test_cases.hpp
@@ -38,4 +38,34 @@ constexpr std::pair<uint64_t, uint64_t> popcount64_test_cases[]{
     {0x007f8001007f8001, 18},
     {0x0055ffaa0055ffaa, 32},
 };
+
+constexpr std::pair<uint32_t, uint32_t> countl_zero32_test_cases[]{
+    {0, 32},
+    {1, 31},
+    {0x7f, 25},
+    {0x80, 24},
+    {0x12345678, 3},
+    {0xffffffff, 0},
+    {0xffff0000, 0},
+    {0x0000ffff, 16},
+    {0x00ffff00, 8},
+    {0x00ff00ff, 8},
+    {0x007f8001, 9},
+    {0x0055ffaa, 9},
+};
+
+constexpr std::pair<uint64_t, uint64_t> countl_zero64_test_cases[]{
+    {0, 64},
+    {1, 63},
+    {0x7f, 57},
+    {0x80, 56},
+    {0x1234567890abcdef, 3},
+    {0xffffffffffffffff, 0},
+    {0xffffffff00000000, 0},
+    {0x00000000ffffffff, 32},
+    {0x0000ffffffff0000, 16},
+    {0x00ff00ff00ff00ff, 8},
+    {0x007f8001007f8001, 9},
+    {0x0055ffaa0055ffaa, 9},
+};
 }  // namespace fizzy::test
